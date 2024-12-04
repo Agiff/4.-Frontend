f04a8af33190d40e1f0ffc5199105b920db97cbe
Agiff <ghifarimohammadrahadian@gmail.com>
2024-12-04 20:48:39 +0700

    feat: adding hover, active, and disabled to card layout

--- 3. weekend-quiz-component/src/App.jsx
+++ 3. weekend-quiz-component/src/App.jsx
@@ -4,20 +4,20 @@ import CardLayout from "./components/CardLayout";
 function App() {
   return (
     <div className="flex flex-col min-h-screen items-center px-12 py-8 gap-10">
-      <h1 className="font-bold text-5xl">React Component</h1>
+      <h1 className="font-bold text-4xl">React Component</h1>
       <div className="flex flex-col gap-5">
         <Button>Primary</Button>
         <Button disabled>Primary</Button>
         <Button variant="secondary">Secondary</Button>
         <Button variant="secondary" disabled>Secondary</Button>
       </div>
-      <CardLayout>
+      <CardLayout disabled>
         <div className="w-[224px] h-[112px] flex justify-center items-center">
           Content area
         </div>
       </CardLayout>
       <CardLayout>
-        <div className="w-[112px] h-[224px] flex justify-center items-center">
+        <div className="w-[112px] h-[180px] flex justify-center items-center">
           Content area
         </div>
       </CardLayout>
--- 3. weekend-quiz-component/src/components/CardLayout.jsx
+++ 3. weekend-quiz-component/src/components/CardLayout.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from "react";
 
-const CardLayout = ({ children }) => {
+const CardLayout = ({ children, disabled }) => {
   return (
-    <div role='button'>
-      <div className='p-4 border border-[#CED2D9]'>
-        {children}
-      </div>
-      <div className='h-1 bg-[#CED2D9]' />
-    </div>
-  )
-}
+    <button className="group disabled:opacity-40" disabled={disabled}>
+      <div className="p-4 border border-[#CED2D9] group-active:border-[#155C93] group-disabled:active:border-[#CED2D9]">{children}</div>
+      <div className="h-1 bg-[#CED2D9] group-hover:bg-[#2670A9] group-disabled:bg-[#CED2D9]" />
+    </button>
+  );
+};
 
-export default CardLayout
+export default CardLayout;
