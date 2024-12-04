e60356de2c72e40cc842bb5c4f059f0789867ced
Agiff <ghifarimohammadrahadian@gmail.com>
2024-12-04 20:19:09 +0700

    feat: adding secondary variant to button

--- 3. weekend-quiz-component/src/App.jsx
+++ 3. weekend-quiz-component/src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen items-center px-12 py-8 gap-10">
       <h1 className="font-bold text-5xl">React Component</h1>
-      <Button>Button</Button>
+      <Button>Primary</Button>
+      <Button disabled>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondary" disabled>Secondary</Button>
     </div>
   );
 }
--- 3. weekend-quiz-component/src/components/Button.jsx
+++ 3. weekend-quiz-component/src/components/Button.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { capitalizeAllLetters } from '../helpers/stringModifier'
 
-const Button = ({ children, disabled }) => {
+const Button = ({ children, disabled, variant = 'primary' }) => {
+  const buttonStyle = {
+    primary: 'bg-[#C5C9D0] hover:bg-[#D9DDE3] active:bg-[#61656E] text-[#161616] active:text-white disabled:active:text-[#161616] disabled:hover:bg-[#C5C9D0] disabled:active:bg-[#C5C9D0]',
+    secondary: 'bg-[#3A362F] hover:bg-[#26221C] active:bg-[#9E9A91] text-[#E9E9E9] active:text-black disabled:active:text-[#E9E9E9] disabled:hover:bg-[#3A362F] disabled:active:bg-[#3A362F]'
+  };
+
+  const renderButton = (variant) => {
+    return buttonStyle[variant]
+  }
+
   return (
-    <button disabled={disabled} className='bg-[#C5C9D0] hover:bg-[#D9DDE3] active:bg-[#61656E] text-[#161616] active:text-white disabled:active:text-[#161616] disabled:opacity-40 min-w-[80px] min-h-[28px] px-2 text-xs'>
+    <button disabled={disabled} className={`${renderButton(variant)} disabled:opacity-40 min-w-[80px] min-h-[28px] px-2 text-xs`}>
       {typeof children === 'string' ? capitalizeAllLetters(children) : children}
     </button>
   )
