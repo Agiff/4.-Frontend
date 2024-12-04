cf4ee8f679d8a831fe433aa4e5694a525c8afb90
Agiff <ghifarimohammadrahadian@gmail.com>
2024-12-04 20:10:37 +0700

    fix: wrong button background

--- 3. weekend-quiz-component/src/components/Button.jsx
+++ 3. weekend-quiz-component/src/components/Button.jsx
@@ -3,7 +3,7 @@ import { capitalizeAllLetters } from '../helpers/stringModifier'
 
 const Button = ({ children, disabled }) => {
   return (
-    <button disabled={disabled} className='bg-[#C5C9D0] hover:bg-[#D9DDE3] active:bg-[#61656E] disabled:bg-[#C5C9D0] text-[#161616] disabled:opacity-40 active:text-white disabled:active:text-[#161616] min-w-[80px] min-h-[28px] px-2 text-xs'>
+    <button disabled={disabled} className='bg-[#C5C9D0] hover:bg-[#D9DDE3] active:bg-[#61656E] text-[#161616] active:text-white disabled:active:text-[#161616] disabled:opacity-40 min-w-[80px] min-h-[28px] px-2 text-xs'>
       {typeof children === 'string' ? capitalizeAllLetters(children) : children}
     </button>
   )
